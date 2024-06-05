ad8fdb45ff54ffff6b68611fdafca55b62ff4e2f
Nikmal8 <kamalcharan404@gmail.com>
2024-06-05 07:12:46 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -33,7 +33,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Nikmal8",
   resume:
-    "https://drive.google.com/file/d/1fsJP2O400X5dayRi0fAl5_-r-eWd0F5t/view?usp=sharing",
+    "https://drive.google.com/file/d/1fzcqJHE2u-ZXBLaKvkxI915MPuv2cGYj/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/kamal-charan21",
 };
 
